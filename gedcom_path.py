649c8ba93110eea63b150300d0e6c0e699b93882
Lars Joost Jensen <ljj@localhost.localdomain>
2019-05-06 17:08:56 +0200

    Made code generic with parameters instead of hardcoded values. Updated to Python3

--- gedcom_path.py
+++ gedcom_path.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python
+
+import sys, getopt
+
 from fuzzywuzzy import process
 
 class Family:
@@ -50,148 +54,133 @@ class Population:
         return None
     def get_identifiers(self):
         return self.individuals.keys()
+    def get_closest_match(self, name):
+        return process.extractOne(name, self.get_names())[0]
+    def get_children(self, identifier):
+        return self.get_individual(identifier).children
+    def get_name_and_id(self, identifier):
+        return self.get_name(identifier) + "(" + identifier + ")"
+    def print_info(self, identifier, print_family=False):
+        print(self.get_individual(identifier).name)
+    def search_tree(self, id, descendent_id):
+        found_descendent = False
+        if descendent_id == id:
+            print("# Family branch start")
+            self.print_info(id)
+            found_descendent = True
+        else:
+            children = self.get_children(id)
+            if children is not None:
+                for child in children:
+                    if self.search_tree(child, descendent_id):
+                        self.print_info(id)
+                        found_descendent = True
+        return found_descendent
+    def print_path(self, ancester_name, descendent_name):
+        found_ancester = self.get_closest_match(ancester_name)
+        ancester_id = self.get_identifier(found_ancester)
+        print("Searched for ancester " + ancester_name + ". Found " + found_ancester + " with id " + ancester_id) 
+        ancester_children = self.get_children(ancester_id)
+        if ancester_children is not None:
+            print("Ancester children = " + str(ancester_children))
+        found_descendent = self.get_closest_match(descendent_name)
+        descendent_id = self.get_identifier(found_descendent)
+        print("Searched for descendent " + descendent_name + ". Found " + found_descendent + " with id " + descendent_id)
+        if self.search_tree(ancester_id, descendent_id):
+            self.print_info(ancester_id)
+        else:
+            print("Could not find descendent " + descendent_name + " of ancester " + ancester_name) 
+        self.print_info(ancester_id, True)
 
-class AST:
-    def __init__(self):
-        self.elements = []
-    def get_element(self, level):
-        while level >= 0:
-            element = self.element.last()
-            level -= 1
-        return element
+class FileParser:
+    def parse_file(self, content, population):
+        current_individual = None
+
+        families = {}
+        current_family = None
+
+        line_number = 0
+
+        for line in content:
+            line_number += 1
+            words = line.split()
+            try:
+                if words[0] == "0":
+                    current_individual = None
+                    current_family = None
+                    identifier = words[1]
+                    if words[2] == "INDI":
+                        current_individual = Individual(identifier)
+                        population.add_individual(current_individual)
+                    elif words[2] == "FAM":
+                        current_family = Family(identifier, population)
+                        families[identifier] = current_family
+            except IndexError:
+                pass
+            if current_individual is not None and len(words) > 1:
+                if words[1] == "NAME":
+                    name = line.split(' ', 2)[2].strip()
+                    name = name.replace('/','')
+                    current_individual.name = name
+                elif words[1] == "FAMS":
+                    current_individual.families.append(words[2])
+                elif words[1] == "FAMC":
+                    assert current_individual.parent_family is None
+                    current_individual.parent_family = words[2]
+            elif current_family is not None and len(words) > 1:
+                if words[1] == "CHIL":
+                    #if len(current_family.children) > 10:
+                    #    raise Exception, "Line " + str(line_number) + ": Family " + str(families)
+                    current_family.children.append(words[2])
+                elif words[1] == "HUSB":
+                    current_family.husbond = words[2]
+                elif words[1] == "WIFE":
+                    current_family.wife = words[2]
+
+        for i in families.keys():
+            family = families[i]
+            population.add_children(family.husbond, family.children)
+            population.add_children(family.wife, family.children)
             
-    def add(self, level, first_word, second_word, rest):
-        pass
-        
-fname = '55dkh1_3530818cc9j55ydc45ader.ged'
 
-with open(fname) as f:
-    content = f.readlines()
 
-population = Population()
-
-current_individual = None
-
-families = {}
-current_family = None
-
-line_number = 0
-
-for line in content:
-    line_number += 1
-    words = line.split()
+def usage():
+    print('gedcom_path.py -f <filename> -a <name> -d <name>')
+    
+def main(argv):
+    inputfile = None
+    ancester_name = None
+    descendent_name = None
     try:
-        if words[0] == "0":
-            current_individual = None
-            current_family = None
-            identifier = words[1]
-            if words[2] == "INDI":
-                current_individual = Individual(identifier)
-                population.add_individual(current_individual)
-            elif words[2] == "FAM":
-                current_family = Family(identifier, population)
-                families[identifier] = current_family
-    except IndexError:
-        pass
-    if current_individual is not None and len(words) > 1:
-        if words[1] == "NAME":
-            name = line.split(' ', 2)[2].strip()
-            name = name.replace('/','')
-            current_individual.name = name
-        elif words[1] == "FAMS":
-            current_individual.families.append(words[2])
-        elif words[1] == "FAMC":
-            assert current_individual.parent_family is None
-            current_individual.parent_family = words[2]
-    elif current_family is not None and len(words) > 1:
-        if words[1] == "CHIL":
-            #if len(current_family.children) > 10:
-            #    raise Exception, "Line " + str(line_number) + ": Family " + str(families)
-            current_family.children.append(words[2])
-        elif words[1] == "HUSB":
-            current_family.husbond = words[2]
-        elif words[1] == "WIFE":
-            current_family.wife = words[2]
+        opts, args = getopt.getopt(argv,"hf:a:d:",["ifile=","ofile="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage()
+            sys.exit()
+        elif opt in ("-f", "--ifile"):
+            inputfile = arg
+        elif opt == "-a":
+            ancester_name = arg
+        elif opt == "-d":
+            descendent_name = arg
 
+    if inputfile is None:
+        print("Input file missing")
+        usage()
+        sys.exit(2)
+       
+    with open(inputfile, 'r', errors='replace') as f:
+        content = f.readlines()
 
-for i in families.keys():
-    family = families[i]
-    population.add_children(family.husbond, family.children)
-    population.add_children(family.wife, family.children)
-            
-# i = "@I500324@"
-
-# individual = population.get_individual(i)
-
-# print individual.children
-
-# exit(1)
+    population = Population()
+    file_parser = FileParser()
+    file_parser.parse_file(content, population)
+    if ancester_name is not None and descendent_name is not None:
+        population.print_path(ancester_name, descendent_name)
     
-#ancester_name = "Carl Frederik Andreas Julius"
+if __name__ == "__main__":
+   main(sys.argv[1:])
 
-#ancester_name = "Casper Thede Keifler"        
-
-#ancester_name = "Anna Elisabeth Storm"        
-
-#ancester_name = "Marie Kirstine Poulsen Vedal"
-
-#ancester_name = "Peder Knudsen Storm"
-#ancester_name = "Peder Nissen Vedel"
-#ancester_name = "Hans Laugesen"
-#ancester_name = "Ebbe Skjalmsen Hvide"
-# ancester_name = "Valdemar Bernhard Joost"
-
-#ancester_name = "Ragnar Lodbrok"
-
-ancester_name = "Hans Tausen"
-
-descendent_name = "Anne Joost Jensen"
-
-found_ancester = process.extractOne(ancester_name, population.get_names())[0]
-
-ancester_id = population.get_identifier(found_ancester)
-
-print "Searched for ancester", ancester_name, ". Found", found_ancester, "with id", ancester_id 
-
-def get_children(identifier):
-    return population.get_individual(identifier).children
-
-def get_name_and_id(identifier):
-    return get_name(identifier) + "(" + identifier + ")"
-
-ancester_children = get_children(ancester_id)
-
-if ancester_children is not None:
-    print "Ancester children =", ancester_children
-    
-found_descendent = process.extractOne(descendent_name, population.get_names())[0]
-
-descendent_id = population.get_identifier(found_descendent)
-
-print "Searched for descendent", descendent_name, ". Found", found_descendent, "with id", descendent_id
-
-def print_info(identifier, print_family=False):
-    print population.get_individual(identifier).name
-    
-def search_tree(id):
-    found_descendent = False
-    if descendent_id == id:
-        print "# Family branch start"
-        print_info(id)
-        found_descendent = True
-    else:
-        children = get_children(id)
-        if children is not None:
-            for child in children:
-                if search_tree(child):
-                    print_info(id)
-                    found_descendent = True
-    return found_descendent
-    
-
-if search_tree(ancester_id):
-    print_info(ancester_id)
-else:
-    print "Could not find descendent", descendent_name, "of ancester", ancester_name 
-
-print_info(ancester_id, True)
